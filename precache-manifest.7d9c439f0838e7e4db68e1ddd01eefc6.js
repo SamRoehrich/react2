ac6da0232c7c2709a348f1cb0a85dd839bad8ae7
SamRoehrich <sam_roehrich@icloud.com>
2019-12-29 17:25:06 -0700

    Updates

---
+++ precache-manifest.7d9c439f0838e7e4db68e1ddd01eefc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf713f071b37befd23d9cd6b5015c22",
+    "url": "/react2/index.html"
+  },
+  {
+    "revision": "2295bcdcf526552d7153",
+    "url": "/react2/static/css/main.b5b11d2e.chunk.css"
+  },
+  {
+    "revision": "4227168f355b4b29eb9d",
+    "url": "/react2/static/js/2.0893cb2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react2/static/js/2.0893cb2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2295bcdcf526552d7153",
+    "url": "/react2/static/js/main.d3ad7de5.chunk.js"
+  },
+  {
+    "revision": "6a035b6e1b2556da1e26",
+    "url": "/react2/static/js/runtime-main.f81af2d3.js"
+  }
+]);
